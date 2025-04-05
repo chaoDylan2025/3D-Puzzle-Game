d70e0f1dbfff30df40577bf20e1705952785cce5
chaoDylan2025 <chaod@mail.gvsu.edu>
2025-04-05 17:40:55 -0400

    added SwiftUI project

---
+++ SwiftUI-3D-Puzzle-Game/SwiftUI-3D-Puzzle-Game/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftUI-3D-Puzzle-Game
+//
+//  Created by Dylan Chao on 4/5/25.
+//
+
+import SwiftUI
+import SceneKit
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-3D-Puzzle-Game/SwiftUI-3D-Puzzle-Game/SwiftUI_3D_Puzzle_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_3D_Puzzle_GameApp.swift
+//  SwiftUI-3D-Puzzle-Game
+//
+//  Created by Dylan Chao on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_3D_Puzzle_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
