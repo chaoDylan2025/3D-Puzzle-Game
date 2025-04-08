eb4ed8dc8fc817f8cca6f787bc6f15bcf20f5874
chaoDylan2025 <chaod@mail.gvsu.edu>
2025-04-08 15:54:27 -0400

    attempting to add scenekit framework

---
+++ SwiftUI-3D-Puzzle-Game/SwiftUI-3D-Puzzle-Game/LevelSelection.swift
@@ -0,0 +1,18 @@
+//
+//  LevelSelection.swift
+//  SwiftUI-3D-Puzzle-Game
+//
+//  Created by Dylan Chao on 4/8/25.
+//
+
+import SwiftUI
+
+struct LevelSelection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LevelSelection()
+}
