553babb7202aa9ad9c53bdad4fa4d70a98f78553
chaoDylan2025 <chaod@mail.gvsu.edu>
2025-04-08 22:45:35 -0400

    copied code from old Swift UI branch and made new level 1 scene

---
+++ SwiftUI-3D-Puzzle-Game/SwiftUI-3D-Puzzle-Game/ContentView.swift
@@ -0,0 +1,48 @@
+import SwiftUI
+import SceneKit
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            ZStack{
+                VStack (spacing: 60){
+                    // Game Title
+                    Text("3D Puzzle Game")
+                    .font(.largeTitle)
+                    .bold()
+                    .navigationBarTitleDisplayMode(.inline)
+                    .padding([.top, .bottom], 100)
+                    
+                    NavigationLink(destination: LevelSelection(), label: {
+                        VStack {
+                            Text("Play")
+                            .frame(width: 60)
+                            .padding(.horizontal, 50)
+                            .padding(.vertical, 15)
+                            .background(Color.green)
+                            .foregroundColor(.white)
+                            .clipShape(Capsule())
+                        }
+                    })
+                    
+                    Button("Setting"){
+                        
+                    }
+                    .frame(width: 60)
+                    .padding(.horizontal, 50)
+                    .padding(.vertical, 15)
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .clipShape(Capsule())
+                    Spacer()
+                }
+            }
+            .containerRelativeFrame([.horizontal, .vertical])
+            .background(Color.red.opacity(0.6))
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-3D-Puzzle-Game/SwiftUI-3D-Puzzle-GameTests/SwiftUI_3D_Puzzle_GameTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_3D_Puzzle_GameTests.swift
+//  SwiftUI-3D-Puzzle-GameTests
+//
+//  Created by Dylan Chao on 4/8/25.
+//
+
+import Testing
+@testable import SwiftUI_3D_Puzzle_Game
+
+struct SwiftUI_3D_Puzzle_GameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
